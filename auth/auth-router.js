07497b94db3d0ca6296169ace7ee673a2c0fe635
Emmanuel Padilla <manolin923@gmail.com>
2020-11-13 10:10:17 -0800

    forgot to commit, register and login working, test almost working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,64 @@
 const router = require('express').Router();
+const bcrypt=require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const Users = require("../users/users-model")
+const { isValid } = require("../users/users-service")
+
+const {jwtSecret } = require('./secrets')
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const credentials =req.body
+  if(isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS ||8
+    const hash = bcrypt.hashSync(credentials.password, rounds)
+    credentials.password = hash
+
+    Users.add(credentials)
+    .then(user => {
+      res.status(201).json({data: user})
+    })
+    .catch(error => {
+      res.status(500).json({message: error.message})
+    })
+  } else {
+    res.status(400).json({
+      message: "please provide username and password"
+    })
+  }
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username, password } = req.body
+
+  if(isValid(req.body)) {
+    Users.findBy({username: username})
+      .then(([user])=>{
+        if (user && bcrypt.compareSync(password, user.password)){
+          const token = makeToken(user)
+          res.status(200).json({ message: "Wilkomen meinen damen und heren!", token })
+        } else {
+          res.status(401).json({ message: "you got no cred"})
+        }
+      })
+      .catch(err =>{
+        res.status(500).json({message: error.message })
+      })
+  }else {
+    res.status(400).json({ message: 'provide user/pass'})
+  }
 });
+
+function makeToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  }
+  const options={
+    expiresIn: '120 seconds'
+  }
+  return jwt.sign(payload, jwtSecret, options)
+}
+
 
 module.exports = router;
---
+++ auth/auth-router.test.js
@@ -0,0 +1,56 @@
+const { default: expectCt } = require('helmet/dist/middlewares/expect-ct')
+const request = require('supertest')
+const server = require('../api/server')
+const db = require('../database/dbConfig')
+const jwt = require('jsonwebtoken');
+
+describe('auth router', () => {
+
+// this will truncate and reset db
+  beforeEach(async () => {
+    await db('users').truncate()
+  })
+
+  describe('testing registration', () => {
+    it('should add new user', async () => {
+      const testOne = await db('users')
+      expect(testOne).toHaveLength(0)
+
+      await request(server).post("/api/auth/register").send({
+          username: "bart",
+          password: "simpson"
+      })
+      const testTwo = await db('users')
+      expect(testTwo).toHaveLength(1)
+
+      await db('users').truncate() //for good measure
+    })
+  })
+})
+  describe('testing POST loggin', async () => {
+    it('allows users to login', async () => {
+      const loginTestOne = await db('users')
+      expect(loginTestOne).toHaveLength(0)
+
+      await request(server).post("api/auth/register").send({
+          username: 'bart',
+          password: "simpson"
+      })
+      const loginTestTwo = await db('users')
+      expect(loginTestTwo).toHaveLength(1)
+      expect(loginTestTwo[0].username).toMatch('bart')
+      const token = res.body.token
+
+      let response = await request(server). post('/api/auth/login').send({
+          username:'bart',
+          password : 'simpson'
+      })
+    //   expect(response).objectContaining(token)
+    })
+  })
+
+
+
+
+
+
